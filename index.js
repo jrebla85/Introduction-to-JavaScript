183eb1711937efc7bfdeee39530681e82e7d68ce
jeremy black <jrebla85@yahoo.com>
2020-12-20 21:16:56 -0600

    Completed Task 7, need to revisit task 6 and 4

--- index.js
+++ index.js
@@ -259,9 +259,30 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
+function grade(score){
+  if (grade >= 90 && grade <=100)
+    {
+      return 'you got an A'
+    }
+  else if (score >= 80 && score <= 89)
+    {
+      return 'you got a B'
+    }
+  else if (score >= 70 && score <= 79)
+    {
+      return 'you got a C'
+    }
+  else if (score >= 60 && score <= 69)
+    {
+      return 'you got a D'
+    }
+  else  
+    {
+      return 'you got an F'
+    } 
   }
+
+  console.log(grade())
   
   
 
