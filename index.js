c4447c0b9c7b46157065aa07b463c552dab10762
jeremy black <jrebla85@yahoo.com>
2020-12-20 18:55:13 -0600

    Finished task 3

--- index.js
+++ index.js
@@ -128,9 +128,44 @@ Use the hungryDog function and feeding requirements below to do the following:
   NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
-    /*add your code here*/
+function hungryDog(weight, age){
+  if (age => 1)
+  {
+    if (weight <= 5) 
+        {
+        return weight * .05
+        }
+      else if (weight >= 6 && weight <= 10)
+        {
+        return weight * .04
+        }
+      else if (weight >= 11 && weight <= 15) 
+        {
+        return weight * .03
+        }
+      else if (weight >= 15) 
+        {
+        return weight * .02
+        }
   }
+  if (age < 1) 
+  {
+    if (age >= .166 && age < 0.333)
+        {
+        return weight * .1
+        }
+      else if (age >= .333  && age < .583)
+        {
+        return weight * .05
+        }
+      else if (age >= .583 && age > 1)
+        {
+        return weight * .04
+        }
+  }
+}
+
+console.log(hungryDog(15,1))
 
 
 
